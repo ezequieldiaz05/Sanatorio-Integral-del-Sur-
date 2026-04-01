18bc385d6e628b4cdb4804b10b764b8b26981423
Ezequiel <gezequield@gmail.com>
2026-03-31 21:36:47 -0300

    Separación en carpetas

--- src/Main/App.java
+++ src/Main/App.java
@@ -1,5 +1,8 @@
+package Main;
+import implementaciones.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
     }
 }
